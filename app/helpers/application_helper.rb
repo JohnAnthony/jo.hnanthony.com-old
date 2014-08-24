02ea20797bd92fcb143ebfba964cefbc11f48d51
John Anthony <john@jo.hnanthony.com>
2014-08-24 16:19:02 +0100

    Added automatic title generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Returns the full title of the page
+  def full_title(page_title)
+    ret = "John Anthony"
+    if not page_title.empty?
+      ret = ret + " | " + page_title
+    end
+    return ret
+  end 
 end
