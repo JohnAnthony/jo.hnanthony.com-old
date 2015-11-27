a9642939602b8ca2acb0c63286f365628d5d2536
John Anthony <john@jo.hnanthony.com>
2015-11-27 21:48:01 +0000

    Serving up styled HTML from an express server

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('build', ['sass', 'jade']);
 
@@ -19,6 +20,11 @@ gulp.task('jade', function() {
 });
 
 gulp.task('watch', ['build'], function() {
-    gulp.watch('./src/index.html', ['html'])
+    gulp.watch('./src/index.jade', ['jade'])
     gulp.watch('./src/sass/*.scss', ['sass'])
+
+	nodemon({
+		script: 'server.js',
+		watch: 'server.js'
+	});
 });
