cc90714078da6f32111a4f709e6c5f666f94e985
John Anthony <john@jo.hnanthony.com>
2015-11-27 20:12:41 +0000

    First working node.js build with gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('build', ['sass', 'jade']);
+
+gulp.task('sass', function() {
+    gulp.src('./src/sass/*.scss')
+        .pipe(concat('bundle.css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('jade', function() {
+    gulp.src('./src/module.html')
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch('./src/index.html', ['html'])
+    gulp.watch('./src/sass/*.scss', ['sass'])
+});
